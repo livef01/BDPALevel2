38ed6a77fe88cad293b454632467fcd4010d992b
Jalen Johnson <jvj2001@gmail.com>
2017-03-04 13:56:49 -0600

    I pratice functions

--- assignments/superhero/code/superhero.js
+++ assignments/superhero/code/superhero.js
@@ -1,3 +1,67 @@
 function showSecretIdentity(){
     alert("I'm a button that doesn't do anything!!");
 }
+
+function time() {
+var time = new Date();
+ var date =  time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+ 
+ return add("The ", "Time:") + date;
+}
+
+function add ( a , b){
+	return a + b ;
+}
+var hello = add("hello", "World")
+
+
+
+var y = 7
+
+function multiply ( a , b){
+	return a * b ;
+}
+
+var x = multiply (y, 10)
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+/*
+var y = 10
+var z = 5;
+
+function add ( a , b){
+	return a + b ;
+}
+
+
+function add ( 10 , 6){
+	return 10 + 6;
+}
+
+var twenty = add(10,10)
+
+var tenPlus6 = add(10 , 6);
+var sixteen = 16;
+
+console.log(x);
+
+var j = 7
+var k = 4
+
+function subtract ( a , b) {
+	return a - b ;
+}
+
+subtract(7 , 4);
+
+console.log(x) ;
+*/
